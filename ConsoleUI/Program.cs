672e4f2b618241f3811943f7d0b4fad46f0719b1
Steven De Roover <steven.deroover@remmicom.be>
2017-05-16 09:05:08 +0200

    Test app cleanup

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,30 +13,7 @@ namespace ConsoleUI
         [STAThread]
         static void Main(string[] args)
         {
-            //Console.WindowHeight = Console.LargestWindowHeight;
-            //Console.WindowWidth = Console.LargestWindowWidth;
-            int index = 0;
-            while (true)
-            {
-                var input = System.Console.ReadKey(true);
-
-                if (index++ < 10)
-                {
-                    System.Console.Write(input.KeyChar);
-                }
-
-                if (input.Key == ConsoleKey.Enter)
-                    break;
-            }
-
-            System.Console.Write("TEST");
-            System.Console.WriteLine();
-            System.Console.WriteLine();
-            System.Console.WriteLine("Press any key to continue...");
-            System.Console.ReadKey();
-            var test = ConsoleReaderX.ReadFromBuffer((short)0, (short)0, (short)5, (short)1).ToList();
-            ConsoleReaderX.DrawRect(5,5,40,10);
-            System.Console.ReadKey();
+            //test app to be created 
         }
     }
 }
