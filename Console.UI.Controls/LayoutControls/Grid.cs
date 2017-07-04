8d60bce2e8c5f0f0bae104a0008604add97e171e
Steven De Roover <steven.deroover@remmicom.be>
2017-07-04 10:03:15 +0200

    Added preliminary grid

--- Console.UI.Controls/LayoutControls/Grid.cs
+++ Console.UI.Controls/LayoutControls/Grid.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Console.UI.Controls.LayoutControls
 {
-    class Grid
+    public class Grid : Control
     {
+        private ObservableCollection<Control> _children = new ObservableCollection<Control>();
+        public ReadOnlyObservableCollection<Control> Children{ get { return new ReadOnlyObservableCollection<Control>(_children); } }
+        public override void Render()
+        {
+            base.Render();
+
+            var g = CreateGraphics();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        public void AddChild(Control control, int row, int column)
+        {
+
+        }
     } 
 }
